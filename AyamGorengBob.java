32551e6ce6a45e4d4149c1635a26f607b67b1ac3
Zickri0 <52669319+Zickri0@users.noreply.github.com>
2024-11-14 17:10:21 +0800

    Chapter 1
    Task

---
+++ AyamGorengBob.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ayamgorengbob;
+
+/**
+ *
+ * @author zickr
+ */
+public class AyamGorengBob {
+    public static void main(String[] args) {
+            System.out.println("----------------------------------------");
+            System.out.println("|      Welcome to Ayam Goreng Bob      |");
+            System.out.println("----------------------------------------");
+            System.out.println("|            List of Menu              |");
+            System.out.println("|      1. Ayam Goreng        : RM5.00  |");
+            System.out.println("|      2. Kentang Goreng     : RM2.50  |");
+            System.out.println("|      3. Air Buah           : RM3.00  |");           
+            System.out.println("________________________________________");
+            
+            System.out.println("\nInsert code of your desired menu : 1\n");
+            System.out.println("Insert quantity : 2\n");            
+            System.out.println("Ayam Goreng : RM5.00\n");
+            System.out.println("Total Price : RM10.00");
+    }
+}
---
+++ HelloWorld.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hello.world;
+
+/**
+ *
+ * @author zickr
+ */
+//Question 1
+/*public class HelloWorld {
+    public static void main(String[] args) { //Only 1 Main Function
+        System.out.println("Hello World!!"); //Display Output
+    }
+    //End of main
+}*/
+//End of HelloWorld Class
+
+//Question 2 & 3
+/*public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("My name is Zickri");
+        System.out.print("I like Teh Tarik"); 
+        System.out.print("My favorite place is my home");
+    }
+}*/
+
+//Question 4
+/*public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("My dream salary is RM50K\nMy dream job is Software Develepor\nMy favorite food is Durian");
+    }
+}*/
+
+//Question 5
+public class HelloWorld {
+    public static void main(String[] args) {
+        int tambah = (5 + 12);
+        int tolak = (3 - 1);
+        int bahagi = (5 / 5);
+        int darab = (12 * 12);
+            System.out.println("===== THIS IS ARTITHMETICS OPERATORS =====\n");
+            System.out.println("          1. 5 + 12 = "+tambah);
+            System.out.println("          2. 3 - 1 = "+tolak);
+            System.out.println("          3. 5 / 5 = "+bahagi);
+            System.out.println("          4. 12 X 12 = "+darab);
+            System.out.println("==========================================");
+    }
+}
